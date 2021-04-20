219d7ad4c7ca45c9b2524bf901959053fef1c9ec
Gustavo Cevallos <wgcv@users.noreply.github.com>
2021-04-20 13:38:59 -0500

    Update ambassadors-map.js

--- assets/js/ambassadors-map.js
+++ assets/js/ambassadors-map.js
@@ -15,7 +15,9 @@ var map = L.map('map');
 L.tileLayer('https://api.tiles.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox.streets',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoid2djdiIsImEiOiJjazNocnhzNGEwMHFhM2VuMW8zbmx4aDZkIn0.I1noSw_DI9HJ8YXrz6tdng'
 }).addTo(mymap);
 mymap.scrollWheelZoom.disable()
